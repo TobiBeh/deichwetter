3f2839502ff9aa80e4c41ae97ff76609d1a2edbe
Tobias Behrendt <tobi@tobiass-mbp.speedport.ip>
2024-11-26 10:40:06 +0100

    Initial commit: Setup Angular project with first components and Tailwind CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { HourlyForecastComponent } from './hourly-forecast/hourly-forecast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    HeaderComponent,
+    CurrentWeatherComponent,
+    HourlyForecastComponent,
+  ],
+  template: `
+    <app-header></app-header>
+    <div class="p-4 space-y-4">
+      <app-current-weather></app-current-weather>
+      <app-hourly-forecast></app-hourly-forecast>
+    </div>
+  `,
 })
-export class AppComponent {
-  title = 'deichwetter';
-}
+export class AppComponent {}
